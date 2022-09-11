a7b0f568d76982582a0de6807d155ccb138b55aa
Titas <belekas2013@gmail.com>
2022-09-11 15:37:37 +0300

    Add project files.

---
+++ SuggestionAppLibrary/Models/BasicSuggestionModel.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver.Core.Operations;
+
+namespace SuggestionAppLibrary.Models;
+public class BasicSuggestionModel
+{
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public BasicSuggestionModel()
+    {
+            
+    }
+
+    public BasicSuggestionModel(SuggestionModel suggestion)
+    {
+        Id = suggestion.Id;
+        Title = suggestion.Suggestion;
+    }
+}
